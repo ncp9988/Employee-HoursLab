9e4701fb3a670cf8b71910aa755080c719ed9142
ncp9988 <ncp9988@gmail.com>
2022-11-08 11:32:12 -0600

    able get whole String info

--- Employee-HoursLab/main.cpp
+++ Employee-HoursLab/main.cpp
@@ -10,7 +10,43 @@
 #include <string>
 using namespace std;
 
-int main() {
+
+// get department name
+
+// add up hours
+
+// add up number of employee
+
+// get part-time employee
+
+// get full-time employee
+
+void getString()
+{
+    ifstream inputfile;
+    inputfile.open("hours.txt");
+    string line;
+    getline(inputfile, line);
+    getline(inputfile, line);
+    getline(inputfile, line);
+    getline(inputfile, line);
+    getline(inputfile, line);
+    getline(inputfile, line);
     
+    
+    string wholeString;
+    
+    while (wholeString != "EOF") {
+        // do something with the line
+        getline(inputfile, wholeString);
+        cout << wholeString << endl;
+
+    }
+    
+}
+
+int main() {
+    getString();
+
     return 0;
 }
