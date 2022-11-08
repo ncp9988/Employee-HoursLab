73b7ceda0cc2423eb6d050fb6bae3a94f7a89793
ncp9988 <ncp9988@gmail.com>
2022-11-07 22:05:49 -0600

    setting up header

--- Employee-HoursLab/main.cpp
+++ Employee-HoursLab/main.cpp
@@ -6,9 +6,11 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    
     return 0;
 }
